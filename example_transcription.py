50a53c9db70540f9d88b93ea698f18cdb7b5cef0
Karan Narula <narulaskaran@gmail.com>
2020-07-16 12:53:59 -0700

    example w sample audio

--- example_transcription.py
+++ example_transcription.py
@@ -1,10 +1,11 @@
 import numpy as np
-import sklearn
 import speech_recognition as sr
 
 print("hello, world!")
 
 r = sr.Recognizer()
-harvard = sr.AudioFile("./harvard.wav")
+harvard = sr.AudioFile("./audio/harvard.wav")
 with harvard as source:
     audio = r.record(source)
+
+print(r.recognize_google(audio))
