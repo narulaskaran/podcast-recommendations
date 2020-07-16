c4da78c311a2c8abc2ff4d2e8147de92ce585b97
Karan Narula <ksn@fb.com>
2020-07-16 12:15:06 -0700

    make files

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sklearn
+import speech_recognition as sr
+from os import path
+from pydub import AudioSegment
+import ffmpeg as ff
+import pocket
+
+print("hello, world!")
