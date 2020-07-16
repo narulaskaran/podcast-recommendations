2bf49d1758ebf6a3a99d7a7139519dcb097fd415
Karan Narula <narulaskaran@gmail.com>
2020-07-16 12:49:45 -0700

    init w harvard.wav

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import sklearn
 import speech_recognition as sr
-from os import path
-from pydub import AudioSegment
-import ffmpeg as ff
-import pocket
 
 print("hello, world!")
+
+r = sr.Recognizer()
+harvard = sr.AudioFile("./harvard.wav")
+with harvard as source:
+    audio = r.record(source)
