2e332127d171fd183675c30a0b6083798c923917
Karan Narula <narulaskaran@gmail.com>
2020-07-17 00:08:44 -0700

    GET endpoint for API

--- web-service/service.py
+++ web-service/service.py
@@ -1,10 +1,22 @@
-from flask import Flask
+# flask run --host=0.0.0.0 --port=8008
+
+from flask import Flask, request, abort
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'To use this recommendation service, send a <strong>GET</strong> request to <strong>x.x.x.x:8008/fetch-related?category=YOUR-CATEGORY</strong>'
 
-@app.route('/<category>')
-def sample_endpoint(category):
-    return 'Fetching podcasts related to {}'.format(category)
+@app.route('/fetch-related')
+def sample_endpoint(methods=['GET']):
+    if request.method != 'GET':
+        abort(400)
+
+    res = json.loads('{}')
+    res['message'] = 'Podcasts related to {}'.format(request.args['category'])
+    podcasts = []
+    # -- CALL CATEOGRIZATION ALGORITHM AND ADD ALL RELATED PODCASTS --
+    # -- insert code here --
+    res['podcasts'] = podcasts
+    return res
